749c49785f3d1bd5dad54b2560cc0947b4a55d22
Nataliia Antonova <5555482@gmail.com>
2023-07-10 11:29:38 +0100

    Adding dark mode

--- frontend/src/routes/settings.rs
+++ frontend/src/routes/settings.rs
@@ -150,15 +150,24 @@ pub fn settings() -> Html {
                         </label>
                     </div>
                     <Pwd onchange={ pwd_onchange.clone() } readonly={ !*editing } required={ !user_password.is_empty() }/>
-                    <div class="relative flex justify-center sm:text-base">
+                    <div class="relative flex space-x-2.5 justify-center sm:text-base">
                         <a href="/login"
                             class={ LINK_CSS }
                             onclick={ onclick_logout.clone() }
                             >
                             { Locale::current().logout() }
                         </a>
+                    // <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
                     </div>
-                </div>
+                    <div class="relative flex space-x-2.5 justify-center sm:text-base">
+                    <label for="toggle"><i class="fas fa-moon icon"></i>{"Dark mode"}</label>
+                    <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+                    <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"/>
+                    <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+                    </div>
+                    </div>
+</div>
+                
             </BlankPage>
         </form>
     }
