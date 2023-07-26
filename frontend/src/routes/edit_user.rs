fd1a4cfcbd24677e1c27da1ef6d47ab17d9a2602
Nataliia Antonova <5555482@gmail.com>
2023-07-26 15:23:24 +0100

    Profile info page

--- frontend/src/routes/edit_user.rs
+++ frontend/src/routes/edit_user.rs
@@ -1,12 +1,33 @@
+use common::error::AppError;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
+use yew_hooks::prelude::*;
+use yew_router::prelude::*;
 
-use crate::components::blank_page::BlankPage;
+use crate::{
+    components::{blank_page::BlankPage, list_errors::ListErrors},
+    css::*,
+    hooks::use_user_context,
+    i18n::*,
+    model::*,
+    services, BaseRoute,
+};
 
 #[function_component(EditUser)]
 pub fn edit_user() -> Html {
     html! {
-      <BlankPage >
-        <></>
-      </BlankPage>
-    }
+              <BlankPage show_footer=true >
+        <div class={ BODY_DIV_CSS }>
+        <div class="text-center">
+        <img
+        src="https://tecdn.b-cdn.net/img/new/avatars/2.webp"
+        class="mx-auto mb-4 w-32 rounded-full"
+        alt="Avatar" />
+      <h5 class="mb-2 text-xl font-medium leading-tight">{"Name Surname"}</h5>
+      <p class="text-neutral-500 dark:text-neutral-400">{"Student"}</p>
+      <p class="text-neutral-500 dark:text-neutral-400">{"Some other info"}</p>
+    </div>
+        </div>
+              </BlankPage>
+            }
 }
