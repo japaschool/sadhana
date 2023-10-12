551bced2b1d80506e52698b198ffd8f5c251b051
Alex Antonov <3091455@gmail.com>
2023-10-12 09:46:52 +0000

    [WIP] Monthly calendar

--- frontend/src/components/calendar.rs
+++ frontend/src/components/calendar.rs
@@ -16,93 +16,82 @@ pub struct Props {
 
 pub const DATE_FORMAT: &'static str = "%Y-%m-%d";
 
+const DATE_CSS: &'static str =
+    "flex group rounded-full mt-2 mx-1 transition-all duration-300 cursor-pointer justify-center";
+
+pub const HOVER_TODAY_DATE_COLOR_CSS: &'static str = "hover:bg-amber-400 dark:hover:bg-amber-400";
+pub const HOVER_DATE_COLOR_CSS: &'static str = "hover:bg-zinc-300 dark:hover:bg-slate-800";
+pub const SELECTED_TODAY_DATE_COLOR_CSS: &'static str = "bg-amber-400";
+pub const SELECTED_DATE_COLOR_CSS: &'static str = "border-2 border-amber-400";
+
 #[function_component(Calendar)]
 pub fn calendar(props: &Props) -> Html {
     let today: NaiveDate = Local::now().date_naive();
-    let selected_date = use_state(|| props.selected_date);
-    let week = use_state(|| vec![Local::now().date_naive()]);
 
-    {
-        let week = week.clone();
-        let callback = props.date_onchange.clone();
-        use_effect_with_deps(
-            move |d| {
-                callback.emit(**d);
-                let d = d.week(Weekday::Mon).first_day();
-                let mut res = vec![d.clone()];
-                for i in 1..7 {
-                    res.push(d.clone().checked_add_days(Days::new(i)).unwrap());
-                }
-                week.set(res);
-                || ()
-            },
-            selected_date.clone(),
-        );
-    }
+    let week = {
+        let d = props.selected_date.week(Weekday::Mon).first_day();
+        let mut res = vec![d.clone()];
+        for i in 1..7 {
+            res.push(d.clone().checked_add_days(Days::new(i)).unwrap());
+        }
+        res
+    };
 
     let onclick_date = {
-        let selected_date = selected_date.clone();
+        let cb = props.date_onchange.clone();
         Callback::from(move |ev: MouseEvent| {
             let input: HtmlInputElement = ev.target_unchecked_into();
             let new_date = NaiveDate::parse_from_str(input.id().as_str(), DATE_FORMAT).unwrap();
-            selected_date.set(new_date);
+            cb.emit(new_date);
         })
     };
 
     let next_week_onclick = {
-        let selected_date = selected_date.clone();
+        let cb = props.date_onchange.clone();
+        let selected_date = props.selected_date.clone();
         Callback::from(move |_: MouseEvent| {
             if selected_date.weekday() == Weekday::Sun {
-                selected_date.set(selected_date.succ_opt().unwrap());
+                cb.emit(selected_date.succ_opt().unwrap());
             } else {
-                selected_date.set(selected_date.checked_add_days(Days::new(7)).unwrap());
+                cb.emit(selected_date.checked_add_days(Days::new(7)).unwrap());
             }
         })
     };
 
     let prev_week_onclick = {
-        let selected_date = selected_date.clone();
+        let cb = props.date_onchange.clone();
+        let selected_date = props.selected_date.clone();
         Callback::from(move |_: MouseEvent| {
             if selected_date.weekday() == Weekday::Mon {
-                selected_date.set(selected_date.pred_opt().unwrap());
+                cb.emit(selected_date.pred_opt().unwrap());
             } else {
-                selected_date.set(selected_date.checked_sub_days(Days::new(7)).unwrap());
+                cb.emit(selected_date.checked_sub_days(Days::new(7)).unwrap());
             }
         })
     };
 
     let ondblclick = {
-        let selected_date = selected_date.clone();
+        let cb = props.date_onchange.clone();
         Callback::from(move |_: MouseEvent| {
-            selected_date.set(today);
+            cb.emit(today);
         })
     };
 
-    const HOVER_TODAY_DATE_DIV_CSS: &'static str = 
-        "flex group dark:hover:bg-amber-400 rounded-full mt-2 mx-1 transition-all duration-300 cursor-pointer justify-center h-8 w-8";
-    const HOVER_DATE_DIV_CSS: &'static str = 
-        "flex group hover:bg-zinc-300 dark:hover:bg-slate-800 rounded-full mt-2 mx-1 transition-all duration-300 cursor-pointer justify-center h-8 w-8";
-    const SELECTED_TODAY_DATE_DIV_CSS: &'static str = 
-        "flex group text-white bg-amber-400 rounded-full mt-2 mx-1 cursor-pointer justify-center h-9 w-9";
-    const SELECTED_DATE_DIV_CSS: &'static str = 
-        "flex group text-white rounded-full border-2 border-amber-400 mt-2 mx-1 cursor-pointer justify-center h-9 w-9";
-
     let calendar_day = |for_selected_date: bool, d: &NaiveDate| -> Html {
         let date_css = match (for_selected_date, *d == today) {
-            (true, true) => SELECTED_TODAY_DATE_DIV_CSS,
-            (true, false) => SELECTED_DATE_DIV_CSS,
-            (false, true) => HOVER_TODAY_DATE_DIV_CSS,
-            (false, false) => HOVER_DATE_DIV_CSS,
+            (true, true) => format!("{SELECTED_TODAY_DATE_COLOR_CSS} h-9 w-9"),
+            (true, false) => format!("{SELECTED_DATE_COLOR_CSS} h-8 w-8"),
+            (false, true) => format!("{HOVER_TODAY_DATE_COLOR_CSS} h-8 w-8"),
+            (false, false) => format!("{HOVER_DATE_COLOR_CSS} h-8 w-8"),
         };
-        let weekday_label_css = if for_selected_date {
-            "text-zinc-500  dark:text-zinc-100 text-xs font-semibold"
-        } else {
-            "text-zinc-500  dark:text-zinc-100 text-xs"
-        };
+        let mut weekday_label_css = "text-zinc-500 dark:text-zinc-100 text-xs".into();
+        if for_selected_date {
+            weekday_label_css = format!("{weekday_label_css} font-semibold");
+        }
         let date_label_css = if for_selected_date {
-            "text-zinc-500  dark:text-zinc-100 my-auto font-bold"
+            "text-zinc-500 dark:text-zinc-100 my-auto font-bold"
         } else {
-            "text-zinc-500  dark:text-zinc-100 dark:group-hover:text-white group-hover:text-zinc-100 my-auto group-hover:font-bold transition-all duration-300"
+            "text-zinc-500 dark:text-zinc-100 dark:group-hover:text-white group-hover:text-zinc-100 my-auto group-hover:font-bold transition-all duration-300"
         };
 
         let id = d.format(DATE_FORMAT);
@@ -116,7 +105,7 @@ pub fn calendar(props: &Props) -> Html {
         html! {
             <div id={id.to_string()} class="text-center">
                 <p id={id.to_string()} class={ weekday_label_css }>{ &Locale::current().day_of_week(d).chars().nth(0).unwrap() }</p>
-                <div id={id.to_string()} class={ date_css } onclick={ onclick_date.clone() }>
+                <div id={id.to_string()} class={ format!("{DATE_CSS} {date_css}") } onclick={ onclick_date.clone() }>
                     {highlight}
                     <p id={id.to_string()} class={ date_label_css }>{ d.format("%-d") }</p>
                 </div>
@@ -125,7 +114,7 @@ pub fn calendar(props: &Props) -> Html {
     };
 
     html! {
-        <div class="relative" {ondblclick}>
+        <div class="relative" {ondblclick} >
             <div class="pb-5 flex justify-center overflow-x-scroll mx-auto max-w-sm">
                 <div class="flex text-zinc-500 dark:text-zinc-100 group w-16" onclick={ prev_week_onclick.clone() }>
                     <div class="flex items-center"><i class="icon-chevron-left"></i></div>
@@ -133,7 +122,7 @@ pub fn calendar(props: &Props) -> Html {
                 {for week.iter().map(|d| html! {
                     <div class="flex group justify-center w-16">
                         <div class="flex items-center">
-                        { calendar_day(*d == *selected_date, d) }
+                        { calendar_day(*d == props.selected_date, d) }
                         </div>
                     </div>
                 })}
