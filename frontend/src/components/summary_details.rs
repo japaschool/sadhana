8c05b842b689bd33b1eada5401c981ce11118720
Alex Antonov <3091455@gmail.com>
2024-05-24 18:14:33 +0000

    Collapsible groups redesign & help page content

--- frontend/src/components/summary_details.rs
+++ frontend/src/components/summary_details.rs
@@ -3,23 +3,22 @@ use yew::prelude::*;
 #[derive(Properties, PartialEq)]
 pub struct Props {
     pub label: AttrValue,
-    pub tab_index: u8,
     pub children: Children,
 }
 
 #[function_component(SummaryDetails)]
 pub fn summary_details(props: &Props) -> Html {
     html! {
-        <div class="group" tabindex={props.tab_index.to_string()}>
-            <div class="group flex justify-between px-4 py-2 items-center transition cursor-pointer pr-10 relative">
-                <div class="items-center inline-flex justify-center rotate-180 transform transition group-focus:-rotate-90 absolute left-0 mb-auto ml-auto">
+        <details class="group">
+            <summary class="group flex justify-between px-4 py-2 items-center transition cursor-pointer pr-10 relative">
+                <div class="items-center inline-flex justify-center rotate-180 transform transition group-open:-rotate-90 absolute left-0 mb-auto ml-auto">
                     <i class="icon-chevron-left"></i>
                 </div>
                 <div class="transition pl-4 hover:opacity-50">{props.label.to_string()}</div>
-            </div>
-            <div class="group-focus:max-h-screen focus-within:max-h-screen max-h-0 px-4 overflow-hidden">
+            </summary>
+            <div class="group-open:max-h-screen focus-within:max-h-screen max-h-0 px-4 overflow-hidden">
                 {props.children.clone()}
             </div>
-        </div>
+        </details>
     }
 }
