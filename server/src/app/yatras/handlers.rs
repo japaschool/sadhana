37b043fda60588edaafdb8c9cd8934e365b1c7f4
Alex Antonov <3091455@gmail.com>
2026-01-05 14:44:03 +0000

    Restructured yatras server module

---
+++ server/src/app/yatras/handlers.rs
@@ -0,0 +1,340 @@
+use actix_web::{HttpRequest, HttpResponse, web};
+use common::error::AppError;
+
+use crate::middleware::{auth, state::AppState};
+
+use crate::app::yatras::{model, dto, stats};
+
+/// Gets yatra data for a cob date
+pub async fn yatra_data(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    params: web::Query<dto::YatraDataQueryParams>,
+    path: web::Path<dto::YatraIdSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let user_id = auth::get_current_user(&req)?.id;
+    let cob_date = params.cob_date;
+    let yatra_id = path.into_inner();
+
+    let res = web::block(move || {
+        match (
+            model::YatraPractice::get_ordered_yatra_practices(&mut conn, &yatra_id),
+            model::YatraDataRow::get_yatra_data(&mut conn, &yatra_id, &cob_date),
+            stats::YatraStatisticResult::get_stats(&mut conn, &user_id, &yatra_id, &cob_date),
+        ) {
+            (Ok(data), Ok(practices), Ok(stats)) => Ok((data, practices, stats)),
+            (Err(e), _, _) | (_, Err(e), _) | (_, _, Err(e)) => {
+                log::warn!("Failed to retrieve yatra data: {e}");
+                Err(e)
+            }
+        }
+    })
+    .await??;
+
+    log::debug!("Yatra data: {:?}", res);
+
+    Ok(HttpResponse::Ok().json(dto::YatraDataResponse::from(res)))
+}
+
+pub async fn is_admin(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    path: web::Path<dto::YatraIdSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let yatra_id = path.into_inner();
+    let user_id = auth::get_current_user(&req)?.id;
+
+    let res = web::block(move || model::Yatra::is_admin(&mut conn, &user_id, &yatra_id)).await??;
+
+    Ok(HttpResponse::Ok().json(dto::YatraIsAdminResponse { is_admin: res }))
+}
+
+pub async fn join_yatra(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    path: web::Path<dto::YatraIdSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let yatra_id = path.into_inner();
+    let user_id = auth::get_current_user(&req)?.id;
+
+    web::block(move || model::Yatra::join(&mut conn, &user_id, &yatra_id)).await??;
+
+    Ok(HttpResponse::Ok().json(()))
+}
+
+pub async fn yatra_leave(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    path: web::Path<dto::YatraIdSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let yatra_id = path.into_inner();
+    let user_id = auth::get_current_user(&req)?.id;
+
+    web::block(move || model::Yatra::leave(&mut conn, &user_id, &yatra_id)).await??;
+
+    Ok(HttpResponse::Ok().json(()))
+}
+
+/// Gets all user yatras
+pub async fn user_yatras(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let user_id = auth::get_current_user(&req)?.id;
+    let yatras = web::block(move || model::Yatra::get_user_yatras(&mut conn, &user_id)).await??;
+
+    Ok(HttpResponse::Ok().json(dto::YatrasResponse { yatras }))
+}
+
+/// Create a new yatra
+pub async fn create_yatra(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    form: web::Json<dto::CreateYatraForm>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let name = form.name.clone();
+    let user_id = auth::get_current_user(&req)?.id;
+
+    let yatra = web::block(move || model::Yatra::create(&mut conn, name, &user_id)).await??;
+
+    Ok(HttpResponse::Ok().json(dto::YatraResponse { yatra }))
+}
+
+/// Delete yatra
+pub async fn delete_yatra(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    path: web::Path<dto::YatraIdSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let user_id = auth::get_current_user(&req)?.id;
+    let yatra_id = path.into_inner();
+
+    web::block(move || model::Yatra::delete(&mut conn, &user_id, &yatra_id)).await??;
+
+    Ok(HttpResponse::Ok().json(()))
+}
+
+/// Rename yatra
+pub async fn update_yatra(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    form: web::Json<dto::UpdateYatraForm>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let user_id = auth::get_current_user(&req)?.id;
+    let yatra = form.into_inner().yatra;
+
+    web::block(move || yatra.update(&mut conn, &user_id)).await??;
+
+    Ok(HttpResponse::Ok().json(()))
+}
+
+/// Get yatra
+pub async fn get_yatra(
+    state: web::Data<AppState>,
+    path: web::Path<dto::YatraIdSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let yatra_id = path.into_inner();
+
+    let yatra = web::block(move || model::Yatra::get_yatra(&mut conn, &yatra_id)).await??;
+
+    Ok(HttpResponse::Ok().json(dto::YatraResponse { yatra }))
+}
+
+/// Get yatra practices
+pub async fn get_yatra_practices(
+    state: web::Data<AppState>,
+    path: web::Path<dto::YatraIdSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let yatra_id = path.into_inner();
+
+    let practices =
+        web::block(move || model::YatraPractice::get_ordered_yatra_practices(&mut conn, &yatra_id))
+            .await??;
+
+    Ok(HttpResponse::Ok().json(dto::YatraPracticesResponse { practices }))
+}
+
+/// Get yatra users
+pub async fn get_yatra_users(
+    state: web::Data<AppState>,
+    path: web::Path<dto::YatraIdSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let yatra_id = path.into_inner();
+
+    let users = web::block(move || model::YatraUser::get_yatra_users(&mut conn, &yatra_id)).await??;
+
+    Ok(HttpResponse::Ok().json(dto::YatraUsersResponse { users }))
+}
+
+/// Delete yatra user
+pub async fn delete_yatra_user(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    path: web::Path<dto::YatraIdUserIdSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let (yatra_id, user_id) = path.into_inner();
+    let current_user_id = auth::get_current_user(&req)?.id;
+
+    web::block(move || {
+        model::Yatra::ensure_admin_user(&mut conn, &current_user_id, &yatra_id)?;
+        model::Yatra::leave(&mut conn, &user_id, &yatra_id)
+    })
+    .await??;
+
+    Ok(HttpResponse::Ok().json(()))
+}
+
+/// Toggle admin
+pub async fn toggle_is_admin(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    path: web::Path<dto::YatraIdUserIdSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let (yatra_id, user_id) = path.into_inner();
+    let current_user_id = auth::get_current_user(&req)?.id;
+
+    web::block(move || model::Yatra::toggle_is_admin(&mut conn, &current_user_id, &user_id, &yatra_id))
+        .await??;
+
+    Ok(HttpResponse::Ok().json(()))
+}
+
+/// Create yatra practice
+pub async fn create_yatra_practice(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    form: web::Json<dto::CreateYatraPracticeForm>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let user_id = auth::get_current_user(&req)?.id;
+
+    web::block(move || model::YatraPractice::create(&mut conn, &user_id, &form.practice)).await??;
+
+    Ok(HttpResponse::Ok().json(()))
+}
+
+/// Get yatra practice details
+pub async fn get_yatra_practice(
+    state: web::Data<AppState>,
+    path: web::Path<dto::YatraIdPracticeSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let (yatra_id, practice_id) = path.into_inner();
+
+    let practice =
+        web::block(move || model::YatraPractice::get(&mut conn, &yatra_id, &practice_id)).await??;
+
+    Ok(HttpResponse::Ok().json(dto::GetYatraPracticeResponse { practice }))
+}
+
+/// Delete yatra practice
+pub async fn delete_yatra_practice(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    path: web::Path<dto::YatraIdPracticeSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let user_id = auth::get_current_user(&req)?.id;
+    let (yatra_id, practice_id) = path.into_inner();
+
+    web::block(move || model::YatraPractice::delete(&mut conn, &user_id, &yatra_id, &practice_id))
+        .await??;
+
+    Ok(HttpResponse::Ok().json(()))
+}
+
+/// Update yatra practice
+pub async fn update_yatra_practice(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    form: web::Json<dto::UpdateYatraPractice>,
+    path: web::Path<dto::YatraIdPracticeSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let user_id = auth::get_current_user(&req)?.id;
+    let (yatra_id, _) = path.into_inner();
+    let data = form.practice.clone();
+
+    web::block(move || model::YatraPractice::update(&mut conn, &user_id, &yatra_id, &data)).await??;
+
+    Ok(HttpResponse::Ok().json(()))
+}
+
+/// Updates order of yatra practices
+pub async fn update_yatra_practice_order_key(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    path: web::Path<dto::YatraIdSlug>,
+    form: web::Json<dto::UpdateYatraPracticeOrderKeyRequest>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let user_id = auth::get_current_user(&req)?.id;
+    let yatra_id = path.into_inner();
+
+    log::info!("Reorder payload {:?}", form.practices);
+
+    let data = form
+        .practices
+        .iter()
+        .enumerate()
+        .map(|(idx, practice)| dto::UpdateYatraPracticeOrderKey {
+            practice_id: *practice,
+            order_key: idx as i32,
+        })
+        .collect();
+
+    web::block(move || model::YatraPractice::update_order_key(&mut conn, &user_id, &yatra_id, &data))
+        .await??;
+    Ok(HttpResponse::Ok().json(()))
+}
+
+/// Get yatra to user practices mapping
+pub async fn get_yatra_user_practices(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    path: web::Path<dto::YatraIdSlug>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let yatra_id = path.into_inner();
+    let user_id = auth::get_current_user(&req)?.id;
+
+    let practices = web::block(move || {
+        model::YatraUserPractice::get_yatra_user_practices(&mut conn, &user_id, &yatra_id)
+    })
+    .await??;
+
+    Ok(HttpResponse::Ok().json(dto::YatraUserPractices { practices }))
+}
+
+/// Update yatra to user practices mapping
+pub async fn update_yatra_user_practices(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    path: web::Path<dto::YatraIdSlug>,
+    form: web::Json<dto::YatraUserPractices>,
+) -> Result<HttpResponse, AppError> {
+    let mut conn = state.get_conn()?;
+    let yatra_id = path.into_inner();
+    let user_id = auth::get_current_user(&req)?.id;
+    let data = form.practices.clone();
+
+    web::block(move || {
+        model::YatraUserPractice::update_yatra_user_practices(&mut conn, &user_id, &yatra_id, &data)
+    })
+    .await??;
+
+    Ok(HttpResponse::Ok().json(()))
+}
--- server/src/routes.rs
+++ server/src/routes.rs
@@ -1,157 +1,157 @@
 use actix_files::{Files, NamedFile};
 use actix_web::{
-    dev::{ServiceRequest, ServiceResponse},
+    dev::{HttpServiceFactory, ServiceRequest, ServiceResponse},
     web,
 };
 
 use crate::app::{self};
 
+fn api_scope() -> impl HttpServiceFactory {
+    web::scope("/api")
+        .service(
+            web::scope("/users")
+                .service(
+                    web::scope("/confirmation")
+                        .route("/{id}", web::get().to(app::user::api::confirmation_details))
+                        .route("", web::post().to(app::user::api::send_confirmation_link)),
+                )
+                .route("/login", web::post().to(app::user::api::signin))
+                .route("", web::post().to(app::user::api::signup)),
+        )
+        .service(
+            web::scope("/password-reset").route("", web::put().to(app::user::api::reset_password)),
+        )
+        .service(
+            web::scope("/user")
+                .service(
+                    web::scope("/practices")
+                        .route("", web::post().to(app::user_practices::add_new))
+                        .route(
+                            "/reorder",
+                            web::put().to(app::user_practices::update_user_practice_order_key),
+                        )
+                        .route("", web::get().to(app::user_practices::get_user_practices)),
+                )
+                .service(
+                    web::scope("/practice/{practice}")
+                        .route("", web::get().to(app::user_practices::get_user_practice))
+                        .route(
+                            "",
+                            web::delete().to(app::user_practices::delete_user_practice),
+                        )
+                        .route("", web::put().to(app::user_practices::update_user_practice)),
+                )
+                .route("", web::get().to(app::user::api::me))
+                .route("", web::put().to(app::user::api::update_user))
+                .route(
+                    "/password",
+                    web::put().to(app::user::api::update_user_password),
+                ),
+        )
+        .service(
+            web::scope("/share/{user_id}")
+                .route("/user", web::get().to(app::user::api::user_info))
+                .route(
+                    "/practices",
+                    web::get().to(app::shared::get_shared_report_practices),
+                )
+                .route("/reports", web::get().to(app::shared::get_shared_reports))
+                .route("", web::get().to(app::shared::get_shared_report_data)),
+        )
+        .service(
+            web::scope("/yatras")
+                .route("", web::post().to(app::yatras::create_yatra))
+                .route("", web::get().to(app::yatras::user_yatras)),
+        )
+        .service(
+            web::scope("/yatra/{yatra_id}")
+                .route("", web::get().to(app::yatras::get_yatra))
+                .route("", web::delete().to(app::yatras::delete_yatra))
+                .route("", web::put().to(app::yatras::update_yatra))
+                .service(
+                    web::scope("/users")
+                        .route(
+                            "/{user_id}",
+                            web::delete().to(app::yatras::delete_yatra_user),
+                        )
+                        .route(
+                            "/{user_id}/is_admin",
+                            web::put().to(app::yatras::toggle_is_admin),
+                        )
+                        .route("", web::get().to(app::yatras::get_yatra_users)),
+                )
+                .service(
+                    web::scope("/practices")
+                        .route(
+                            "/reorder",
+                            web::put().to(app::yatras::update_yatra_practice_order_key),
+                        )
+                        .route("", web::get().to(app::yatras::get_yatra_practices))
+                        .route("", web::post().to(app::yatras::create_yatra_practice)),
+                )
+                .service(
+                    web::scope("/practice/{practice_id}")
+                        .route("", web::get().to(app::yatras::get_yatra_practice))
+                        .route("", web::put().to(app::yatras::update_yatra_practice))
+                        .route("", web::delete().to(app::yatras::delete_yatra_practice)),
+                )
+                .route("/data", web::get().to(app::yatras::yatra_data))
+                .route(
+                    "/user-practices",
+                    web::get().to(app::yatras::get_yatra_user_practices),
+                )
+                .route(
+                    "/user-practices",
+                    web::put().to(app::yatras::update_yatra_user_practices),
+                )
+                .route("/join", web::put().to(app::yatras::join_yatra))
+                .route("/leave", web::put().to(app::yatras::yatra_leave))
+                .route("/is_admin", web::get().to(app::yatras::is_admin)),
+        )
+        .service(web::scope("/support-form").route("", web::post().to(app::support::send_message)))
+        .service(
+            web::scope("/reports")
+                .route("", web::post().to(app::report::create_report))
+                .route("", web::get().to(app::report::get_reports)),
+        )
+        .service(
+            web::scope("/report/{report_id}")
+                .route("", web::put().to(app::report::update_report))
+                .route("", web::delete().to(app::report::delete_report)),
+        )
+        .service(
+            web::scope("/diary")
+                .route(
+                    "/incomplete-days",
+                    web::get().to(app::diary::api::get_incomplete_days),
+                )
+                .service(
+                    web::scope("/{cob}")
+                        .route("/report", web::get().to(app::diary::api::get_report_data))
+                        .route(
+                            "/entry",
+                            web::put().to(app::diary::api::upsert_diary_day_entry),
+                        )
+                        .route("", web::put().to(app::diary::api::upsert_diary_day))
+                        .route("", web::get().to(app::diary::api::get_diary_day)),
+                ),
+        )
+}
+
+fn dist_files() -> actix_files::Files {
+    Files::new("/", "./dist/")
+        .index_file("index.html")
+        // Redirect back to index.html for paths not found on disk. See https://github.com/actix/actix-web/issues/2115
+        .default_handler(|req: ServiceRequest| {
+            let (http_req, _payload) = req.into_parts();
+            async move {
+                let response_file = NamedFile::open_async("./dist/index.html").await?;
+                let response = response_file.into_response(&http_req);
+                Ok(ServiceResponse::new(http_req, response))
+            }
+        })
+}
+
 pub fn routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::scope("/api")
-            .service(
-                web::scope("/users")
-                    .service(
-                        web::scope("/confirmation")
-                            .route("/{id}", web::get().to(app::user::api::confirmation_details))
-                            .route("", web::post().to(app::user::api::send_confirmation_link)),
-                    )
-                    .route("/login", web::post().to(app::user::api::signin))
-                    .route("", web::post().to(app::user::api::signup)),
-            )
-            .service(
-                web::scope("/password-reset")
-                    .route("", web::put().to(app::user::api::reset_password)),
-            )
-            .service(
-                web::scope("/user")
-                    .service(
-                        web::scope("/practices")
-                            .route("", web::post().to(app::user_practices::add_new))
-                            .route(
-                                "/reorder",
-                                web::put().to(app::user_practices::update_user_practice_order_key),
-                            )
-                            .route("", web::get().to(app::user_practices::get_user_practices)),
-                    )
-                    .service(
-                        web::scope("/practice/{practice}")
-                            .route("", web::get().to(app::user_practices::get_user_practice))
-                            .route(
-                                "",
-                                web::delete().to(app::user_practices::delete_user_practice),
-                            )
-                            .route("", web::put().to(app::user_practices::update_user_practice)),
-                    )
-                    .route("", web::get().to(app::user::api::me))
-                    .route("", web::put().to(app::user::api::update_user))
-                    .route(
-                        "/password",
-                        web::put().to(app::user::api::update_user_password),
-                    ),
-            )
-            .service(
-                web::scope("/share/{user_id}")
-                    .route("/user", web::get().to(app::user::api::user_info))
-                    .route(
-                        "/practices",
-                        web::get().to(app::shared::get_shared_report_practices),
-                    )
-                    .route("/reports", web::get().to(app::shared::get_shared_reports))
-                    .route("", web::get().to(app::shared::get_shared_report_data)),
-            )
-            .service(
-                web::scope("/yatras")
-                    .route("", web::post().to(app::yatras::create_yatra))
-                    .route("", web::get().to(app::yatras::user_yatras)),
-            )
-            .service(
-                web::scope("/yatra/{yatra_id}")
-                    .route("", web::get().to(app::yatras::get_yatra))
-                    .route("", web::delete().to(app::yatras::delete_yatra))
-                    .route("", web::put().to(app::yatras::update_yatra))
-                    .service(
-                        web::scope("/users")
-                            .route(
-                                "/{user_id}",
-                                web::delete().to(app::yatras::delete_yatra_user),
-                            )
-                            .route(
-                                "/{user_id}/is_admin",
-                                web::put().to(app::yatras::toggle_is_admin),
-                            )
-                            .route("", web::get().to(app::yatras::get_yatra_users)),
-                    )
-                    .service(
-                        web::scope("/practices")
-                            .route(
-                                "/reorder",
-                                web::put().to(app::yatras::update_yatra_practice_order_key),
-                            )
-                            .route("", web::get().to(app::yatras::get_yatra_practices))
-                            .route("", web::post().to(app::yatras::create_yatra_practice)),
-                    )
-                    .service(
-                        web::scope("/practice/{practice_id}")
-                            .route("", web::get().to(app::yatras::get_yatra_practice))
-                            .route("", web::put().to(app::yatras::update_yatra_practice))
-                            .route("", web::delete().to(app::yatras::delete_yatra_practice)),
-                    )
-                    .route("/data", web::get().to(app::yatras::yatra_data))
-                    .route(
-                        "/user-practices",
-                        web::get().to(app::yatras::get_yatra_user_practices),
-                    )
-                    .route(
-                        "/user-practices",
-                        web::put().to(app::yatras::update_yatra_user_practices),
-                    )
-                    .route("/join", web::put().to(app::yatras::join_yatra))
-                    .route("/leave", web::put().to(app::yatras::yatra_leave))
-                    .route("/is_admin", web::get().to(app::yatras::is_admin)),
-            )
-            .service(
-                web::scope("/support-form").route("", web::post().to(app::support::send_message)),
-            )
-            .service(
-                web::scope("/reports")
-                    .route("", web::post().to(app::report::create_report))
-                    .route("", web::get().to(app::report::get_reports)),
-            )
-            .service(
-                web::scope("/report/{report_id}")
-                    .route("", web::put().to(app::report::update_report))
-                    .route("", web::delete().to(app::report::delete_report)),
-            )
-            .service(
-                web::scope("/diary")
-                    .route(
-                        "/incomplete-days",
-                        web::get().to(app::diary::api::get_incomplete_days),
-                    )
-                    .service(
-                        web::scope("/{cob}")
-                            .route("/report", web::get().to(app::diary::api::get_report_data))
-                            .route(
-                                "/entry",
-                                web::put().to(app::diary::api::upsert_diary_day_entry),
-                            )
-                            .route("", web::put().to(app::diary::api::upsert_diary_day))
-                            .route("", web::get().to(app::diary::api::get_diary_day)),
-                    ),
-            ),
-    )
-    .service(
-        Files::new("/", "./dist/")
-            .index_file("index.html")
-            // Redirect back to index.html for paths not found on disk. See https://github.com/actix/actix-web/issues/2115
-            .default_handler(|req: ServiceRequest| {
-                let (http_req, _payload) = req.into_parts();
-                async move {
-                    let response_file = NamedFile::open_async("./dist/index.html").await?;
-                    let response = response_file.into_response(&http_req);
-                    Ok(ServiceResponse::new(http_req, response))
-                }
-            }),
-    );
+    cfg.service(api_scope()).service(dist_files());
 }
