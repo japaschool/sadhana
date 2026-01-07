f022a0b3c41f1792703b8b48b146e18c90f939f9
Alex Antonov <3091455@gmail.com>
2026-01-07 15:29:13 +0000

    Precompressing assets

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,4 @@
-use actix_web::{
-    middleware::{Compress, Logger},
-    web::Data,
-    App, HttpServer,
-};
+use actix_web::{App, HttpServer, middleware::Logger, web::Data};
 use diesel_migrations::*;
 use dotenvy::dotenv;
 #[cfg(not(target_env = "msvc"))]
@@ -48,7 +44,6 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .app_data(Data::new(app_state))
             .wrap(middleware::cors::cors())
-            .wrap(Compress::default())
             .wrap(middleware::auth::Authentication)
             .configure(routes::routes)
     })
